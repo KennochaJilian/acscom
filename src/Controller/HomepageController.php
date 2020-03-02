dde31dee66cef6b8f336433abac4ea103ed67eef
Aranxa.C <tassa780@gmail.com>
2020-03-02 14:33:40 +0000

    Possibilite de rechercher un produit depuis la barre de recherche

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Data\Search;
 use App\Data\SearchData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
 use App\Form\SearchForm;
+use App\Form\SearchType;
 use App\Repository\ProductRepository;
-use App\Repository\TagRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends AbstractController
@@ -17,19 +18,23 @@ class HomepageController extends AbstractController
      * @Route("/", name="homepage")
      */
 
-    public function index(ProductRepository $repositery, Request $request, TagRepository $tagRepo)
+    public function index(ProductRepository $repositery, Request $request)
     {
+
         $data =new SearchData(); 
+        
         $form = $this->createForm(SearchForm::class, $data);
+
+        if(!empty($_POST)){
+            $data->q = $_POST['search']; 
+        }
         $form->handleRequest($request); 
-
-        $tags = $tagRepo->findAll();          
-
         $products = $repositery->findSearch($data);
 
         return $this->render('homepage/index.html.twig', [
             'products' => $products,
             'form' => $form->createView()
+           
         ]);
     }
 
